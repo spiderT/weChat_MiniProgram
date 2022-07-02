9d051f050ed238d9734b26af12f70237915dfe50
spiderT <1304126527@qq.com>
2022-07-02 20:45:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# weChat_MiniProgram
+
+练习demo，在TDesign 零售行业模版示例小程序上做了些修改
---
+++ model/swiper.js
@@ -0,0 +1,36 @@
+const images = [
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner1.png',
+    text: '1',
+    bgColor: '#e5e6e7',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner2.png',
+    text: '2',
+    bgColor: '#f7d079',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner3.png',
+    text: '3',
+    bgColor: '#ececec',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner4.png',
+    text: '4',
+    bgColor: '#eecbad',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner5.png',
+    text: '5',
+    bgColor: '#9ed9e3',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner6.png',
+    text: '6',
+    bgColor: '#202228',
+  },
+];
+
+export function genSwiperImageList() {
+  return images;
+}
